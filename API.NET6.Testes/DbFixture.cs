9de80d8d22a8ac134fbf4b9d606cce4d538c03b5
Welington Marquezini Valverde da Silva <welington_marquezini88@live.com>
2023-02-04 23:49:56 -0300

    ajuste na versao .net 6 teste

---
+++ API.NET6.Testes/DbFixture.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using API.NET6.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.NET6.Testes
+{
+	public class DbFixture
+	{
+		public IConfiguration Configuration { get; }
+		public IServiceProvider ServiceProvider { get; private set; }
+		public DbFixture()
+		{
+		}
+
+		public void Init()
+        {
+			var serviceCollection = new ServiceCollection();
+
+			string mySqlConnection = "Server=localhost;Database=api;uid=root;password=";
+			serviceCollection
+				.AddDbContext<DataContext>(
+					options =>
+						options.UseMySql(mySqlConnection,
+					    ServerVersion.AutoDetect(mySqlConnection)
+					)
+				);
+
+			ServiceProvider = serviceCollection.AddLogging().BuildServiceProvider();
+
+        }
+
+		public DataContext GetDataContext() {
+			return ServiceProvider.GetService<DataContext>();
+		}
+	}
+}
+
